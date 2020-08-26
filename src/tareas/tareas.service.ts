4554c38969daefc8706e9005347b878b6a2b0898
GermanBonzi <gbonzi716@gmail.com>
2020-08-26 19:56:12 -0300

    tareas

--- src/tareas/tareas.service.ts
+++ src/tareas/tareas.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { Tarea } from "./interfaces/tareas";
 @Injectable()
-export class TareasService {}
+export class TareasService {
+
+    tareas: Tarea[]=[
+        {
+            id:1,
+            titulo:"tarea 1",
+            descripcion:"testeando tareas",
+            hecho:true
+        },
+        {
+            id:2,
+            titulo:"tarea 2",
+            descripcion:"testeando tareas 2",
+            hecho:true
+        },
+        {
+            id:3,
+            titulo:"tarea 3",
+            descripcion:"testeando tareas 3",
+            hecho:false
+        },
+        {
+            id:1,
+            titulo:"tarea 4",
+            descripcion:"testeando tareas 4",
+            hecho:false
+        }
+    ];
+    
+}
